c6ec6352b2c53e88c23ec68c1a1ca40e24ecec0a
Fillipe626 <lpjp91@gmail.com>
2021-04-20 16:06:59 -0400

    Configurações iniciais

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:buscador_gifs/ui/home_page.dart';
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+
+
+
+/*share: ^0.6.1+1
+transparent_image: ^1.0.0
+http: ^0.12.0+2*/
