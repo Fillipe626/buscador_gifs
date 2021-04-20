75e6db39dce34c25f25d5d172ac4e60be1894912
Fillipe626 <lpjp91@gmail.com>
2021-04-20 17:36:12 -0400

    Modificações na interface

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'package:buscador_gifs/ui/home_page.dart';
 void main(){
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
 
 
 
 
-/*share: ^0.6.1+1
-transparent_image: ^1.0.0
-http: ^0.12.0+2*/
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -8,13 +11,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
 
-  _getGifs() async {
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
     http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=4UxPpzjkqatUd1l6G2WK5V6pwbUWM8Rg&limit=20&rating=r");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=4UxPpzjkqatUd1l6G2WK5V6pwbUWM8Rg&q=$_search&limit=20&offset=$_offset&rating=r&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void iniState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.network(
+              "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: Column(
+            children: <Widget>[
+        Padding(
+        padding: EdgeInsets.all(10.0),
+        child: TextField(
+          decoration: InputDecoration(
+              labelText: "Pesquise Aqui",
+              labelStyle: TextStyle(color: Colors.white),
+              border: OutlineInputBorder()
+          ),
+          style: TextStyle(color: Colors.white, fontSize: 10.0),
+          textAlign: TextAlign.center,
+        ),
+    )
+    ],
+    ),
+    );
   }
 }
